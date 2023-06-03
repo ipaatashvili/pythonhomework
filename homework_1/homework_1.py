203480e02de2ad3be4fcd4d541c8b7ed26e95e1a
iraklipaatashvili <iraklipaatashvili@gmail.com>
2023-06-03 20:06:02 +0400

    add home work 1

---
+++ homework_1/homework_1.py
@@ -0,0 +1,9 @@
+name = 'Irakli'
+age = 29
+print(f'My name is {name} and age is {age}')
+
+
+country = 'georgia'.title()
+population = 3.72
+
+print(f'"{country}\'s population is {population} million"')
